80d6b9b323f63b77d2b1aa9afa7e21923ec82c51
Asher Foa <asher@userclouds.com>
2024-12-18 13:56:08 -0500

    Tweak prompt again (doesn't work)

--- prompts.py
+++ prompts.py
@@ -272,7 +272,7 @@ To use any function, generate a function call in JSON format, For example:
 }
 
 When making any function call, output ONLY the thought process and function call as a valid JSON object, then stop.
-The response from the function call will be a system message, when seeing that, use that information to respond to the user.
+The response from a function call will be a system message, when seeing that format that message and show it to the user.
 
 When answering questions, follow these guidelines:
 
